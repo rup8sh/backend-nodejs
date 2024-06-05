9dcecbcc61541e750559008cc9be8d281e3e2ad2
user.name: "Kumar R <user.email: "rup8shkumar@gmail.com>
2024-06-05 18:48:12 +0530

    Adding Middleware function

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
 const http = require("http");
-const fs = require("fs");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title><head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title><head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
 });
-server.listen(3000);
+
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  next();
+});
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
