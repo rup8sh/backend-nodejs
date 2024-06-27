22c55288559fe7cc806968c8ac5f178ce483ac18
Kumar-R <rup8shkumar@gmail.com>
2024-06-27 19:01:48 +0530

    Final Update
    Final update for backend-nodejs

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,31 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', isAuth, shopController.getCart);
+
+router.post('/cart', isAuth, shopController.postCart);
+
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
+
+router.get('/checkout', isAuth, shopController.getCheckout);
+
+router.get('/checkout/success', shopController.getCheckoutSuccess);
+
+router.get('/checkout/cancel', shopController.getCheckout);
+
+router.get('/orders', isAuth, shopController.getOrders);
+
+router.get('/orders/:orderId', isAuth, shopController.getInvoice);
 
 module.exports = router;
